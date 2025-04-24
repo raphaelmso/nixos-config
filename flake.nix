850c7bfba2378f7faaf55f89a9bf6c395a032d03
Raphael Oliveira <raphael.msoliveira@gmail.com>
2025-04-24 15:25:06 -0300

    initial files

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+
+    description = "Initial flake";
+    inputs = {
+      nixpkgs.url = "nixpkgs/nixos-24.11";
+      home-manager.url = "github:nix-community/home-manager/release-24.11";
+      home-manager.inputs.nixpkgs.follows = "nixpkgs"; 
+         };
+    outputs = { self, nixpkgs, home-manager, ... }: 
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations = {
+        forge = lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+          ];
+        };
+      };
+      homeConfigurations = {
+         raphael = home-manager.lib.homeManagerConfiguration {
+         inherit pkgs;
+          modules = [
+            ./home.nix
+          ];
+        };       
+      };
+    };
+}

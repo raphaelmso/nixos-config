947c44f6a5374d7d9c8739d20e57141c5d81dc7e
Raphael Oliveira <raphael.msoliveira@gmail.com>
2025-05-08 15:50:30 -0300

    add alacritty and zellij

--- home.nix
+++ home.nix
@@ -37,11 +37,12 @@
     # '')
     yazi
     lazygit
-    tmux
     kitty
     starship
     tldr
     nil
+    alacritty
+    zellij
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -79,6 +80,43 @@
     EDITOR = "hx";
   };
 
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window.startup_mode = "Fullscreen";
+      font.normal = {
+        family = "JetBrainsMono Nerd Font";
+        style = "regular";
+      };
+      font.size = 13;
+
+      colors.primary = {
+        background = "#1a1b26";
+        foreground = "#c0caf5";
+      };
+      colors.normal = {
+        black = "#15161e";
+        red = "#f7768e";
+        green = "#9ece6a";
+        yellow = "#e0af68";
+        blue = "#7aa2f7";
+        magenta = "#bb9af7";
+        cyan = "#7dcfff";
+        white = "#a9b1d6";
+      };
+      colors.bright = {
+        black = "#414868";
+        red = "#ff899d";
+        green = "#9fe044";
+        yellow = "#faba4a";
+        blue = "#8db0ff";
+        magenta = "#c7a9ff";
+        cyan = "#a4daff";
+        white = "#c0caf5";
+      };
+    };
+  };
+
   programs.zsh = {
     enable = true;
 
@@ -97,9 +135,7 @@
       plugins = [
         ''
           Multirious/zsh-helix-mode
-          zsh-users/zsh-history-substring-search
           zsh-users/zsh-autosuggestions
-          zsh-users/zsh-syntax-highlighting
           Aloxaf/fzf-tab
         ''
       ]; # explanation of "path:..." and other options explained in Antidote README.
@@ -127,69 +163,6 @@
 
   programs.yazi = {
     enable = true;
-  };
-
-  programs.tmux = {
-    enable = true;
-  };
-
-  programs.kitty = {
-    enable = true;
-    extraConfig = "
-      font_family JetBrainsMono Nerd Font
-      font_size 13.0
-      bold_font auto
-      italic_font auto
-      bold_italic_font auto
-      disable_ligatures always
-
-      # background_opacity 0.85
-      clear_all_mouse_actions yes
-      hide_window_decorations yes
-
-      background #1a1b26
-      foreground #c0caf5
-      selection_background #283457
-      selection_foreground #c0caf5
-      url_color #73daca
-      cursor #c0caf5
-      cursor_text_color #1a1b26
-
-      # Tabs
-      active_tab_background #7aa2f7
-      active_tab_foreground #16161e
-      inactive_tab_background #292e42
-      inactive_tab_foreground #545c7e
-      #tab_bar_background #15161e
-
-      # Windows
-      active_border_color #7aa2f7
-      inactive_border_color #292e42
-
-      # normal
-      color0 #15161e
-      color1 #f7768e
-      color2 #9ece6a
-      color3 #e0af68
-      color4 #7aa2f7
-      color5 #bb9af7
-      color6 #7dcfff
-      color7 #a9b1d6
-
-      # bright
-      color8  #414868
-      color9  #ff899d
-      color10 #9fe044
-      color11 #faba4a
-      color12 #8db0ff
-      color13 #c7a9ff
-      color14 #a4daff
-      color15 #c0caf5
-
-      # extended colors
-      color16 #ff9e64
-      color17 #db4b4b
-    ";
   };
 
   programs.helix = {
